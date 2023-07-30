2a916e965709aa0c83e25becc4e6f4763a22f777
Maxime Huguet~ <huguet.maxime@gmail.com>
2023-07-30 23:04:43 +0200

    ajout style input scroll sur ProfileScreen

--- App.js
+++ App.js
@@ -1,84 +1,80 @@
-import 'react-native-gesture-handler';
-import { useCallback } from 'react';
-
+import "react-native-gesture-handler";
+import { useCallback } from "react";
 
 //Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 // Elements
-import { SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
 
 //font
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 // import * as SplashScreen from 'expo-splash-screen';
 
 //Screens
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-
-
 const AppDrawerNavigation = () => (
-  <Drawer.Navigator 
-  screenOptions={({ route }) => ({
-    headerShown: false,
-    drawerActiveTintColor:'#25958A',
-    drawerInactiveTintColor: '#d95b33',
-    drawerType: 'front',
-    drawerIcon: ({ focused, color, size }) => {
-      let iconName;
-      if (route.name === 'Home') {
-        iconName = focused ? 'home' : 'home'; // Nom de l'icône FontAwesome pour "Home"
-      } else if (route.name === 'Profile') {
-        iconName = focused ? 'user' : 'user'; // Nom de l'icône FontAwesome pour "Profile"
-      }
-      // Définis la couleur en fonction de la variable 'iconColor'
-        iconColor = focused ? '#D95B33' : '#000'; // Mettre '#d95b33' pour la couleur souhaitée
+  <Drawer.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      drawerActiveTintColor: "#25958A",
+      drawerInactiveTintColor: "#d95b33",
+      drawerType: "front",
+      drawerIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === "Home") {
+          iconName = focused ? "home" : "home"; // Nom de l'icône FontAwesome pour "Home"
+        } else if (route.name === "Profile") {
+          iconName = focused ? "user" : "user"; // Nom de l'icône FontAwesome pour "Profile"
+        }
+        // Définis la couleur en fonction de la variable 'iconColor'
+        iconColor = focused ? "#D95B33" : "#000"; // Mettre '#d95b33' pour la couleur souhaitée
 
-      // Retourne l'icône FontAwesome avec le nom calculé
-      return <FontAwesome name={iconName} size={size} color={color} />;
-    },
-  })} 
-  drawerStyle={{
-    backgroundColor: '#FCFAF1',
-    width: '80%', // Ajoute la largeur de 80% ici
-  }}
+        // Retourne l'icône FontAwesome avec le nom calculé
+        return <FontAwesome name={iconName} size={size} color={color} />;
+      },
+    })}
+    drawerStyle={{
+      backgroundColor: "#FCFAF1",
+      width: "80%", // Ajoute la largeur de 80% ici
+    }}
   >
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
+    <Drawer.Screen name="Home" component={HomeScreen} />
+    <Drawer.Screen name="Profile" component={ProfileScreen} />
   </Drawer.Navigator>
 );
 
 export default function App() {
-
-
   const [fontsLoaded] = useFonts({
-    'Outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    Outfit: require("./assets/fonts/Outfit-Regular.ttf"),
   });
-  
-    const onLayoutRootView = useCallback(async () => {
-      if (fontsLoaded) {
-        await SplashScreen.hideAsync();
-      }
-    }, [fontsLoaded]);
-  
-    if (!fontsLoaded) {
-      return null;
-    }
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-      <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="AppDrawerNavigation" component={AppDrawerNavigation} />
-          </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="AppDrawerNavigation"
+          component={AppDrawerNavigation}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,69 +1,164 @@
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Avatar } from "@rneui/themed";
 
 export default function ProfileScreen() {
-
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
+  const [firstname, setFirstname] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
 
   return (
-    <View style={styles.background}>
-        <SafeAreaView style={styles.container}>
-          <View style={styles.header}>
-            <TouchableOpacity                     
-                onPress={() => navigation.openDrawer()}>
-                <FontAwesome 
-                  name={'bars'} 
-                  size={40} 
-                  color={'#25958A'}
-                  />
-            </TouchableOpacity>
-            <Text>ProfileScreen</Text>
+    <KeyboardAvoidingView
+      style={styles.background}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false} >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <FontAwesome name={"bars"} size={40} color={"#25958A"} />
+          </TouchableOpacity>
+          <Text>Retour</Text>
+        </View>
+        <View style={styles.profilAvatar}>
+          <Text style={styles.h1}>Profil</Text>
+          <Image
+            style={styles.imageAvatar}
+            source={require("../assets/avatar-profil.png")}
+          />
+          <Text>@USERNAME</Text>
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.8}
+            //A REDIRIGER VERS LE CALIBRAGE
+            // onPress={()=> }
+          >
+            <Text style={styles.textButton}>Me re-calibrer</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.dataProfil}>
+          <View>
+            <Text style={styles.h3}>Mes coordonnées</Text>
           </View>
-
-          <Text>
-            Profil
-          </Text>
-          <TouchableOpacity 
-          style={styles.button} 
+          <View style={styles.inputProfil}>
+            <TextInput
+              style={styles.input}
+              onChangeText={setFirstname}
+              value={firstname}
+              placeholder="Prénom"
+            ></TextInput>
+            <TextInput
+              style={styles.input}
+              onChangeText={setName}
+              value={name}
+              placeholder="Nom"
+            ></TextInput>
+            <TextInput
+              style={styles.input}
+              onChangeText={setUsername}
+              value={username}
+              placeholder="Nom utilisateur"
+            ></TextInput>
+            <TextInput
+              style={styles.input}
+              onChangeText={setEmail}
+              value={email}
+              placeholder="Email"
+            ></TextInput>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
           activeOpacity={0.8}
           //AJOUTER LA FONCTIONNALITE POUR SAUVEGARDER LES INPUTS DE PROFIL
           // onPress={()=> }
-          >
-            <Text style={styles.textButton}>Sauvegarder</Text>
-          </TouchableOpacity>
+        >
+          <Text style={styles.textButton}>Sauvegarder</Text>
+        </TouchableOpacity>
       </SafeAreaView>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FCFAF1',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
   background: {
     flex: 1,
-    backgroundColor: '#d6d1bd',
+  },
+  scrollView: {
+    marginHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+    // backgroundColor: "#FCFAF1",
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
     padding: 20,
   },
+  profilAvatar: {
+    width: "100%",
+    margin: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  h3: {
+    width: "80%",
+    fontSize: 20,
+    fontWeight: "600",
+    fontFamily: "Outfit",
+    marginBottom: 10,
+  },
+  dataProfil: {
+    alignItems: "center",
+    width: "100%",
+  },
+  inputProfil: {
+    alignItems: "center",
+    width: "100%",
+  },
+  input: {
+    alignItems: "center",
+    height: 40,
+    margin: 8,
+    borderWidth: 1,
+    borderColor: "#D6D1BD",
+    padding: 5,
+    width: "80%",
+    fontFamily: 'Outfit',
+  },
+  h1: {
+    fontFamily: "Outfit",
+    fontSize: 24,
+    marginBottom: 5,
+  },
   button: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingTop: 8,
-    width: '80%',
+    width: "80%",
     marginTop: 30,
-    backgroundColor: '#D6D1BD',
+    backgroundColor: "#D6D1BD",
     borderRadius: 30,
     marginBottom: 80,
     shadowOpacity: 1,
@@ -76,11 +171,15 @@ const styles = StyleSheet.create({
     shadowColor: "rgba(0, 0, 0, 0.25)",
   },
   textButton: {
-    color: '#707B81',
+    color: "#707B81",
     height: 30,
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 16,
-    fontFamily: 'Outfit'
-
+    fontFamily: "Outfit",
+  },
+  imageAvatar: {
+    width: 90,
+    height: 124,
+    marginTop: 15,
   },
 });
