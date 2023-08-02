e7761dfb796dc9eb25b90f7c2c3ac67c5a3180f5
Maxime Huguet~ <huguet.maxime@gmail.com>
2023-08-02 16:44:16 +0200

    tab mensurations

--- screens/CalibrateScreen.js
+++ screens/CalibrateScreen.js
@@ -1,76 +1,172 @@
 import * as React from "react";
-import { useState } from "react";
-
-//font
-import { useFonts } from "expo-font";
-
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Text,
   Dimensions,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const FirstRoute = () => (
-  <View style={styles.firstRoute}>
-    <View>
-      <Text style={styles.h3}>Ajouter une taille</Text>
-    </View>
-    <View></View>
-    <View>
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.8}
-        //AJOUTER LA FONCTIONNALITE POUR PASSER A L'ETAPE SUIVANTE
-        // onPress={()=> }
-      >
-        <Text style={styles.textButton}>Suivant</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+const PremierRoute = ({ onSubmit }) => {
+  const marqueRef = React.useRef(null);
+  const typeRef = React.useRef(null);
+  const coupeRef = React.useRef(null);
+  const tailleRef = React.useRef(null);
 
-const SecondRoute = () => (
-  <View style={{ flex: 1, backgroundColor: "#FCFAF1" }}>
-        <View>
-      <Text style={styles.h3}>Ajouter une taille</Text>
-    </View>
-    <View></View>
-    <View>
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.8}
-        //AJOUTER LA FONCTIONNALITE POUR PASSER A L'ETAPE SUIVANTE
-        // onPress={()=> }
-      >
-        <Text style={styles.textButton}>Suivant</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : null}
+    >
+      <ScrollView keyboardShouldPersistTaps="always">
+        <View style={styles.premierRoute}>
+          {/* ... */}
+          <View style={styles.containerInput}>
+            <ChampInput ref={marqueRef} placeholder="Marque" />
+            <ChampInput ref={typeRef} placeholder="Type" />
+            <ChampInput ref={coupeRef} placeholder="Coupe" />
+            <ChampInput ref={tailleRef} placeholder="Taille" />
+          </View>
+          <View>
+            <TouchableOpacity
+              style={styles.button}
+              activeOpacity={0.8}
+              onPress={() =>
+                onSubmit(
+                  marqueRef.current.value,
+                  typeRef.current.value,
+                  coupeRef.current.value,
+                  tailleRef.current.value
+                )
+              }
+            >
+              <Text style={styles.textButton}>Suivant</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
 
-  
-);
 
-const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
-});
+const SecondRoute = ({ onSubmit }) => {
+    const poitrineRef = React.useRef(null);
+    const tourtailleRef = React.useRef(null);
+    const hancheRef = React.useRef(null)
+    
+ 
+    return (
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : null}
+        >
+          <ScrollView 
+            contentContainerStyle={{flexGrow: 1}}
+            keyboardShouldPersistTaps='never'>
+            <View>
+                <Text style={styles.h3}>Renseigner vos mensurations</Text>
+            </View>
+            <View style={styles.tailleSwitch}>
+                <TouchableOpacity activeOpacity={0.5}>
+                    <Text style={styles.taille}>EU</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.taille}>US</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                 <Text style={styles.taille}>UK</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.secondRoute}>
+              {/* ... */}
+              <View style={styles.containerInput}>
+                <View style={styles.inputBox}>
+                <Text style={styles.texte}>Tour de poitrine</Text>
+                <MensurationsInput ref={poitrineRef} placeholder="cm" />
+                </View>
+                <View style={styles.inputBox}>
+                <Text style={styles.texte}>Tour de taille</Text>
+                <MensurationsInput ref={tourtailleRef} placeholder="cm" />                 
+                </View>
+                <View style={styles.inputBox}>
+                <Text style={styles.texte}>Tour de hanches</Text>
+                <MensurationsInput ref={hancheRef} placeholder="cm" />                    
+                </View>
+              </View>
+              <View>
+                <TouchableOpacity
+                  style={styles.button}
+                  activeOpacity={0.8}
+                  onPress={() =>
+                    onSubmit(
+                      poitrineRef.current.value,
+                      tourtailleRef.current.value,
+                      hancheRef.current.value,
+                    )
+                  }
+                >
+                  <Text style={styles.textButton}>Valider</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      );
+}
+
+
+
+const ChampInput = React.forwardRef((props, ref) => (
+  <TextInput
+    {...props}
+    ref={ref}
+    style={styles.input}
+    onChangeText={(text) => {
+      ref.current.value = text;
+    }}
+  />
+));
+
+const MensurationsInput = React.forwardRef((props, ref) => (
+    <TextInput
+      {...props}
+      ref={ref}
+      style={styles.input}
+      keyboardType="number-pad"
+      maxLength={3}
+      onChangeText={(text) => {
+        ref.current.value = text;
+      }}
+    />
+  ));
 
 export default function CalibrateScreen({ navigation }) {
-  
-  const initialLayout = { width: Dimensions.get("window").width };
-
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
     { key: "first", title: "Tailles" },
     { key: "second", title: "Mensuration" },
   ]);
+
+  const onSubmit = (marque, type, coupe, taille) => {
+    // Traiter les valeurs ici, comme les enregistrer dans une base de données, etc.
+    console.log(marque, type, coupe, taille);
+  };
+
+  const renderScene = SceneMap({
+    first: () => <PremierRoute onSubmit={onSubmit} />,
+    second: SecondRoute,
+  });
+
+  const initialLayout = { width: Dimensions.get("window").width };
 
   return (
     <View style={styles.background}>
@@ -86,14 +182,14 @@ export default function CalibrateScreen({ navigation }) {
         </View>
       </View>
       <TabView
-        navigationState={{ index, routes }} 
+        navigationState={{ index, routes }}
         renderTabBar={(props) => (
           <TabBar
             {...props}
             renderLabel={({ route, color }) => (
               <Text style={{ color: "#FFFF", margin: 8 }}>{route.title}</Text>
             )}
-            style={{ backgroundColor: "#d95b33", fontFamily: "Outfit" }}
+            style={{ backgroundColor: "#d95b33" }}
           />
         )}
         renderScene={renderScene}
@@ -127,24 +223,56 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "600",
     marginBottom: 20,
-    fontFamily: "Outfit",
   },
   tabView: {
     marginTop: 10,
     width: "80%",
     borderRadius: 10,
   },
-  firstRoute: {
+  premierRoute: {
     flex: 1,
     backgroundColor: "#FCFAF1",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  secondRoute: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FCFAF1",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 30,
+    paddingTop: 30,
+  },
+  containerInput: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    width: '100%',
+    backgroundColor: '#fcfaf1',
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  inputBox : {
+    width : '100%'
+  },
+  input: {
+    alignItems: 'flex-start',
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#D6D1BD",
+    padding: 5,
+    marginTop: 10,
+    marginBottom: 10, 
+    width: "100%",
+    borderRadius: 5,
+    backgroundColor: '#ffffff'
   },
   button: {
     width: 150,
     alignItems: "center",
     marginTop: 20,
     paddingTop: 8,
+    marginBottom: 30,
     backgroundColor: "#D6D1BD",
     borderRadius: 30,
     shadowOpacity: 1,
@@ -156,16 +284,27 @@ const styles = StyleSheet.create({
     },
     shadowColor: "rgba(0, 0, 0, 0.25)",
   },
-  h3: {
-    color: "#707B81",
-    fontSize: 20,
-    fontFamily: "Outfit",
-  },
   textButton: {
     color: "#707B81",
     height: 30,
     fontWeight: "600",
     fontSize: 16,
+  },
+  tailleSwitch: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  taille: {
+    color: "#707B81",
+    padding: 15,
+  },
+  h3: {
+    color: "#000000",
+    fontSize: 20,
+    fontWeight: "700",
+    fontFamily: "Outfit",
+  },
+  texte: {
     fontFamily: "Outfit",
   },
 });
