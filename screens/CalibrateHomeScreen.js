161a15eb0c815063a493839e80063575a8e13c97
Maxime Huguet~ <huguet.maxime@gmail.com>
2023-08-06 22:44:32 +0200

    add modal calibrateHomeScreen

--- screens/CalibrateHomeScreen.js
+++ screens/CalibrateHomeScreen.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import {
   StyleSheet,
   View,
@@ -8,20 +9,30 @@ import {
   Dimensions,
   Image,
   SafeAreaView,
+  Modal,
+  TouchableWithoutFeedback,
 } from "react-native";
 import Carousel, { Pagination } from "react-native-snap-carousel";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const carouselItemHeight = 300; // Vous pouvez ajuster cette valeur selon vos besoins
+const url = process.env.EXPO_PUBLIC_IP;
+
+const carouselItemHeight = 240;
 
 export default function CalibrateHomeScreen({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const userToken = useSelector((state) => state.user.value);
+  console.log("ceci est le token", userToken);
+
   const width = Dimensions.get("window").width;
 
   const dataType = [
-    require("../assets/pantalon.png"),
-    require("../assets/t-shirt.png"),
-    require("../assets/shoes.png"),
+    { image: require("../assets/t-shirt.png"), text: "Haut" },
+    { image: require("../assets/pantalon.png"), text: "Bas" },
+    { image: require("../assets/shoes.png"), text: "Chaussures" },
   ];
+
   const [activeIndex, setActiveIndex] = useState(0);
 
   const carouselRef = useRef(null);
@@ -37,24 +48,96 @@ export default function CalibrateHomeScreen({ navigation }) {
         }}
       >
         <Image
-          source={item}
+          source={item.image}
           style={{
             width: "60%",
             height: "60%",
             resizeMode: "contain",
           }}
         />
+        <Text style={{ marginTop: 10, fontSize: 18, fontWeight: "bold" }}>
+          {item.text}
+        </Text>
       </View>
     );
   };
+
+  //Fonction qui permet de connaitre le type sélectionné dans le carrousel
+  const getSelectedType = () => {
+    const selectedItem = dataType[activeIndex];
+    return selectedItem.text.toLowerCase();
+  };
+
+  // Fonction qui permet d'accéder au calibrage d'un type (Haut, bas ou chaussures)
+  const submitCalidrage = () => {
+    const type = getSelectedType();
+
+    console.log(type);
+
+    fetch(`${url}/users/mensurations/${type}/${userToken}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data.result && setModalVisible(true);
+
+        data.error && navigation.navigate("Calibrate");
+      });
+  };
+
+// l'utilisateur souhaite modifier son calibrage malgré l'affichage de la modal indiquant que des mensurations existent
+const forceCalibrate = () => {
+  setModalVisible(false)
+  navigation.navigate("Calibrate")
+}
+
   return (
     <View style={styles.background}>
+      <Modal visible={modalVisible} animationType="fade" transparent>
+        <View style={styles.modalContainer}>
+          <TouchableWithoutFeedback
+            onPress={() => setModalVisible(false)}
+          >
+            <View style={styles.centeredView}>
+              <TouchableWithoutFeedback>
+                <View style={styles.modalView}>
+                  <Image
+                    source={require(`../assets/t-shirt.png`)}
+                    style={styles.image}
+                  />
+                  <Text style={styles.h3}>
+                    Attention ! Vous avez déjà effectué votre calibrage. Souhaitez-vous quand même continuer?
+                  </Text>
+                  <TouchableOpacity 
+                  style={styles.button} 
+                  activeOpacity={0.8}
+                  onPress={() => forceCalibrate()}
+                  >
+                    <Text style={styles.textButton}>
+                      Modifier mon calibrage
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.button2}
+                    activeOpacity={0.8}
+                    onPress={() => setModalVisible(false)}
+                  >
+                    <Text style={styles.textButton2}>
+                      Annuler
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </Modal>
       <SafeAreaView style={styles.header}>
         <View style={styles.burgerIcon}>
           <TouchableOpacity onPress={() => navigation.openDrawer()}>
             <FontAwesome name={"bars"} size={40} color={"#25958A"} />
           </TouchableOpacity>
         </View>
+
         <View style={styles.titleContainer}>
           <Text style={styles.H1}>Calibrage</Text>
           <View style={styles.border}></View>
@@ -70,56 +153,55 @@ export default function CalibrateHomeScreen({ navigation }) {
             </Text>
           </View>
         </View>
-        <View style={styles.carouselContainer}>
-          <TouchableOpacity onPress={() => carouselRef.current.snapToPrev()}>
-            <FontAwesome
-              name={"chevron-left"}
-              size={40}
-              color={"#d95b33"}
-              marginLeft={20}
-              marginRight={10}
-            />
-          </TouchableOpacity>
+        <View style={styles.containerInput}>
+          <View style={styles.carouselContainer}>
+            <TouchableOpacity onPress={() => carouselRef.current.snapToPrev()}>
+              <FontAwesome
+                name={"chevron-left"}
+                size={40}
+                color={"#d95b33"}
+                marginLeft={20}
+                marginRight={10}
+              />
+            </TouchableOpacity>
 
-          <Carousel
-            ref={carouselRef}
-            data={dataType}
-            renderItem={_renderItem}
-            sliderWidth={width}
-            itemWidth={width} // Adjust according to your needs
-            onSnapToItem={(index) => setActiveIndex(index)}
-          />
-          <TouchableOpacity onPress={() => carouselRef.current.snapToNext()}>
-            <FontAwesome
-              name={"chevron-right"}
-              size={40}
-              color={"#d95b33"}
-              marginRight={20}
-              marginLeft={10}
+            <Carousel
+              ref={carouselRef}
+              data={dataType}
+              renderItem={_renderItem}
+              sliderWidth={width}
+              itemWidth={width} // Adjust according to your needs
+              onSnapToItem={(index) => setActiveIndex(index)}
             />
-          </TouchableOpacity>
+            <TouchableOpacity onPress={() => carouselRef.current.snapToNext()}>
+              <FontAwesome
+                name={"chevron-right"}
+                size={40}
+                color={"#d95b33"}
+                marginRight={20}
+                marginLeft={10}
+              />
+            </TouchableOpacity>
           </View>
-
-
 
           <Pagination
             dotsLength={dataType.length}
             activeDotIndex={activeIndex}
-            containerStyle={{ backgroundColor: "rgba(0, 0, 0, 0.75)" }}
             dotStyle={styles.paginationDot} // spécifie le style des indicateurs individuels
             inactiveDotOpacity={0.4}
             inactiveDotScale={0.6}
           />
-          <View style={styles.btn}>
+          <View>
             <TouchableOpacity
               style={styles.button}
               activeOpacity={0.8}
               //AJOUTER LA FONCTIONNALITE POUR PASSER A L'ETAPE SUIVANTE
-              // onPress={()=> }
+              onPress={() => submitCalidrage()}
             >
-              <Text style={styles.textButton}>Continuer</Text>
+              <Text style={styles.textButton}>Valider</Text>
             </TouchableOpacity>
           </View>
+        </View>
       </SafeAreaView>
     </View>
   );
@@ -212,10 +294,11 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   button: {
-    width: 150,
+    width: 200,
     alignItems: "center",
-    marginTop: 10,
+    marginTop: 20,
     paddingTop: 8,
+    marginBottom: 15,
     backgroundColor: "#d95b33",
     borderRadius: 30,
     shadowOpacity: 1,
@@ -227,9 +310,80 @@ const styles = StyleSheet.create({
     },
     shadowColor: "rgba(0, 0, 0, 0.25)",
   },
-  btn:{
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    marginTop: '15%',
-  }
+  btn: {
+    alignItems: "center",
+    // justifyContent: 'flex-end',
+    // marginTop: '15%',
+  },
+  textButton: {
+    color: "#ffffff",
+    fontFamily: "Outfit",
+    height: 30,
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  button2: {
+    width: 200,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    marginBottom: 15,
+    backgroundColor: "#d6d1bd",
+    borderRadius: 30,
+    shadowOpacity: 1,
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  textButton2: {
+    color: "#707b81",
+    fontFamily: "Outfit",
+    height: 30,
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  containerInput: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#fcfaf1",
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    backgroundColor: "white",
+    width: "90%",
+    borderRadius: 20,
+    padding: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  image: {
+    width: 100,
+    height: 110,
+    borderRadius: 50,
+    marginBottom: 15,
+  },
 });
