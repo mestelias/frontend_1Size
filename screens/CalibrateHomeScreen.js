e629a50b3893b1d23b5da190cc2d208232f97d91
Sacha <Your email>
2023-08-07 10:56:17 +0200

    ...

--- screens/CalibrateHomeScreen.js
+++ screens/CalibrateHomeScreen.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-
+import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import {
   StyleSheet,
   View,
@@ -7,84 +8,197 @@ import {
   Text,
   Dimensions,
   Image,
+  SafeAreaView,
+  Modal,
+  TouchableWithoutFeedback,
 } from "react-native";
+import Carousel, { Pagination } from "react-native-snap-carousel";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { useRef, useState } from "react"
 
+const url = process.env.EXPO_PUBLIC_IP;
 
-import Carousel, { Pagination } from 'react-native-snap-carousel';
+const carouselItemHeight = 240;
 
 export default function CalibrateHomeScreen({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const carouselRef = useRef(null);
-  const initialLayout = Dimensions.get("window").width ;
-  // paramètre clée qui défini sur quel image on s'est arrêté
-  const [activeSlide, setActiveSlide] = useState(0);
-  // tableau qui sert à envoyer la bonne props au CalibrateScreen par rapport à activeSlide
-  const categorie = ["Chaussures","Bas","Haut"]
+  const userToken = useSelector((state) => state.user.value.token);
 
-  const handleContinueButton = () => {
-    navigation.navigate('CalibrateScreen', { categorie: categorie[activeSlide] })
-  }
-  
-  const images = [
-    require("../assets/vetements/basket.png"),
-    require("../assets/vetements/pantalon.jpeg"),
-    require("../assets/vetements/teeshirt.jpeg"),
+  const width = Dimensions.get("window").width;
+
+  const dataType = [
+    { image: require("../assets/t-shirt.png"), text: "haut" },
+    { image: require("../assets/pantalon.png"), text: "bas" },
+    { image: require("../assets/shoes.png"), text: "chaussures" },
   ];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const carouselRef = useRef(null);
+
+
+  const _renderItem = ({ item, index }) => {
+    return (
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          width: "70%",
+          height: carouselItemHeight,
+        }}
+      >
+        <Image
+          source={item.image}
+          style={{
+            width: "60%",
+            height: "60%",
+            resizeMode: "contain",
+          }}
+        />
+        <Text style={{ marginTop: 10, fontSize: 18, fontWeight: "bold" }}>
+          {item.text}
+        </Text>
+      </View>
+    );
+  };
+  
+  const selectedItem = dataType[activeIndex].text;
+  console.log(selectedItem)
+
+  
+  // Fonction qui permet d'accéder au calibrage d'un type (Haut, bas ou chaussures)
+  const submitCalidrage = () => {
+
+    console.log(selectedItem);
+
+    fetch(`${url}/users/mensurations/${selectedItem}/${userToken}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data.result && setModalVisible(true);
+
+        data.error && navigation.navigate("CalibrateScreen");
+      });
+  };
+
+// l'utilisateur souhaite modifier son calibrage malgré l'affichage de la modal indiquant que des mensurations existent
+const forceCalibrate = () => {
+  setModalVisible(false)
+  navigation.navigate("CalibrateScreen")
+}
 
   return (
     <View style={styles.background}>
+      <Modal visible={modalVisible} animationType="fade" transparent>
+        <View style={styles.modalContainer}>
+          <TouchableWithoutFeedback
+            onPress={() => setModalVisible(false)}
+          >
+            <View style={styles.centeredView}>
+              <TouchableWithoutFeedback>
+                <View style={styles.modalView}>
+                  <Image
+                    source={require(`../assets/t-shirt.png`)}
+                    style={styles.image}
+                  />
+                  <Text style={styles.h3}>
+                    Attention ! Vous avez déjà effectué votre calibrage. Souhaitez-vous quand même continuer?
+                  </Text>
+                  <TouchableOpacity 
+                  style={styles.button} 
+                  activeOpacity={0.8}
+                  onPress={() => forceCalibrate()}
+                  >
+                    <Text style={styles.textButton}>
+                      Modifier mon calibrage
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.button2}
+                    activeOpacity={0.8}
+                    onPress={() => setModalVisible(false)}
+                  >
+                    <Text style={styles.textButton2}>
+                      Annuler
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </Modal>
       <SafeAreaView style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.openDrawer()}>
-          <FontAwesome name={"bars"} size={40} color={"#25958A"} />
-        </TouchableOpacity>
-      </SafeAreaView>
-      <Text style={styles.H1}>Calibrage</Text>
-      <View style={styles.container}>
-        <Text style={styles.h3}>Choisis ton type de vêtement à calibrer</Text>
-        <View style={styles.carou}>
-          {/* la fonction snapToPrev du carousel est appelée, ce qui fait défiler le carousel vers l'image précédente */}
-          <TouchableOpacity onPress={() => carouselRef.current.snapToPrev()}>
-          {/* Flèche gauche */}
-            <FontAwesome name={"chevron-left"} size={40} color={"#d95b33"} marginLeft={25} />
-          </TouchableOpacity>
-          <Carousel
-            ref={carouselRef}
-            data={images}
-            renderItem={({ item }) => (
-              <Image source={item} style={styles.image} />
-            )}
-            sliderWidth={initialLayout}
-            itemWidth={initialLayout}
-            containerCustomStyle={styles.carouselContainer}
-            contentContainerStyle={{ height: 200 }}
-            onSnapToItem={(index) => setActiveSlide(index)}
-          />
-          {/* la fonction snapToNext du carousel est appelée, ce qui fait défiler le carousel vers l'image suivante */}
-          <TouchableOpacity onPress={() => carouselRef.current.snapToNext()}>
-            {/* Flèche droite */}
-            <FontAwesome name={"chevron-right"} size={40} color={"#d95b33"} marginRight={20} />
+        <View style={styles.burgerIcon}>
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <FontAwesome name={"bars"} size={40} color={"#25958A"} />
           </TouchableOpacity>
         </View>
-        <Pagination
-          dotsLength={images.length} // spécifie le nombre total d'indicateurs à afficher
-          activeDotIndex={activeSlide} // définit l'index de l'indicateur actif
-          dotStyle={styles.paginationDot} // spécifie le style des indicateurs individuels
-          inactiveDotOpacity={0.4} // définit l'opacité des indicateurs inactifs 
-          inactiveDotScale={0.6} // définit l'échelle des indicateurs inactifs par rapport à l'indicateur actif
-        />
-      </View>
-      <View style={styles.btn}>
-        <TouchableOpacity
-          style={styles.button}
-          activeOpacity={0.8}
-          onPress={handleContinueButton}
-        >
-          <Text style={styles.textButton}>Continuer</Text>
-        </TouchableOpacity>
-      </View>
+
+        <View style={styles.titleContainer}>
+          <Text style={styles.H1}>Calibrage</Text>
+          <View style={styles.border}></View>
+          <View style={styles.paragraphe}>
+            <Text style={styles.h3}>Pour commencer l'aventure</Text>
+            <Text style={styles.one}> 1</Text>
+            <Text style={styles.size}>Size</Text>
+          </View>
+          <View style={styles.paragraphe}>
+            <Text style={styles.text}>
+              Vous devez calibrer au minimum un vêtement parmi la liste suivante
+              :
+            </Text>
+          </View>
+        </View>
+        <View style={styles.containerInput}>
+          <View style={styles.carouselContainer}>
+            <TouchableOpacity onPress={() => carouselRef.current.snapToPrev()}>
+              <FontAwesome
+                name={"chevron-left"}
+                size={40}
+                color={"#d95b33"}
+                marginLeft={20}
+                marginRight={10}
+              />
+            </TouchableOpacity>
+
+            <Carousel
+              ref={carouselRef}
+              data={dataType}
+              renderItem={_renderItem}
+              sliderWidth={width}
+              itemWidth={width} // Adjust according to your needs
+              onSnapToItem={(index) => setActiveIndex(index)}
+            />
+            <TouchableOpacity onPress={() => carouselRef.current.snapToNext()}>
+              <FontAwesome
+                name={"chevron-right"}
+                size={40}
+                color={"#d95b33"}
+                marginRight={20}
+                marginLeft={10}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <Pagination
+            dotsLength={dataType.length}
+            activeDotIndex={activeIndex}
+            dotStyle={styles.paginationDot} // spécifie le style des indicateurs individuels
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+          />
+          <View>
+            <TouchableOpacity
+              style={styles.button}
+              activeOpacity={0.8}
+              //AJOUTER LA FONCTIONNALITE POUR PASSER A L'ETAPE SUIVANTE
+              onPress={() => submitCalidrage()}
+            >
+              <Text style={styles.textButton}>Valider</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
@@ -92,52 +206,95 @@ export default function CalibrateHomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: "#fcfaf1",
+    backgroundColor: "#FCFAF1",
   },
   header: {
-    alignItems: "flex-start",
-    paddingTop: 30,
-    paddingLeft: 20,
-    paddingRight: 20,
+    flex: 1,
+    justifyContent: "flex-start",
+    width: "100%",
   },
-  // container: {
-  //   flex: 0.2,
-  //   alignItems: "center",
-  //   justifyContent: "space-between",
-  //   backgroundColor: 'red'
-  // },
-  container: {
-    height: "50%",
+  H1: {
+    fontSize: 24,
+    fontFamily: "Outfit",
+    fontWeight: "800",
+    marginBottom: 20,
+  },
+  burgerIcon: {
+    flexDirection: "row",
+    paddingLeft: 30,
+    paddingTop: 15,
+  },
+  titleContainer: {
+    alignItems: "center",
+    backgroundColor: "#fcfaf1",
+    marginTop: 30,
+  },
+  border: {
+    // paddingVertical: 10, // Ajoute un padding vertical de 10 pixels autour du texte
+    paddingHorizontal: 35, // Ajoute un padding horizontal de 20 pixels autour du texte
+    borderBottomWidth: 3, // Ajoute une bordure sous le texte
+    borderBottomColor: "#d95b33", // Couleur de la bordure sous le texte
+    borderRadius: 50,
+  },
+  paragraphe: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  one: {
+    fontSize: 20,
+    fontFamily: "Outfit",
+    textAlign: "center",
+    color: "#d95b33",
+  },
+  size: {
+    color: "#707B81",
+    textAlign: "center",
+    fontFamily: "Outfit",
+    fontSize: 20,
+  },
+  h3: {
+    fontSize: 20,
+    fontWeight: "600",
+    fontFamily: "Outfit",
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "Outfit",
+    textAlign: "center",
+  },
+  carouselContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    height: carouselItemHeight, // Ajoutez cette ligne
+  },
+  carouselImageContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  titleBox: {},
-  H1: {
-    textAlign: "center",
-    fontSize: 24,
-    fontWeight: "600",
-    //marginBottom: 20,
-    fontFamily: "Outfit",
-    color: 'black'
-  },
-  tabView: {
-    marginTop: 20,
-    width: "80%",
+  paginationDot: {
+    width: 15,
+    height: 15,
     borderRadius: 10,
-    maxHeight: 100
-
+    marginHorizontal: 8,
+    backgroundColor: "#d95b33",
   },
-  firstRoute: {
-    flex: 1,
-    backgroundColor: "#FCFAF1",
-    alignItems: "center",
-    justifyContent: "space-around",
+  carouselImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
   },
   button: {
-    width: 150,
+    width: 200,
     alignItems: "center",
     marginTop: 20,
     paddingTop: 8,
+    marginBottom: 15,
     backgroundColor: "#d95b33",
     borderRadius: 30,
     shadowOpacity: 1,
@@ -149,48 +306,80 @@ const styles = StyleSheet.create({
     },
     shadowColor: "rgba(0, 0, 0, 0.25)",
   },
-  h3: {
-    color: "#707B81",
-    fontSize: 20,
-    fontFamily: "Outfit",
-    marginBottom: 20
+  btn: {
+    alignItems: "center",
+    // justifyContent: 'flex-end',
+    // marginTop: '15%',
   },
   textButton: {
     color: "#ffffff",
+    fontFamily: "Outfit",
     height: 30,
     fontWeight: "600",
     fontSize: 16,
-    fontFamily: "Outfit",
   },
-  basket : {
-    width: 140,
-    height: 190,
+  button2: {
+    width: 200,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    marginBottom: 15,
+    backgroundColor: "#d6d1bd",
+    borderRadius: 30,
+    shadowOpacity: 1,
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  textButton2: {
+    color: "#707b81",
+    fontFamily: "Outfit",
+    height: 30,
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  containerInput: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#fcfaf1",
+    borderRadius: 10,
+    marginTop: 15,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    backgroundColor: "white",
+    width: "90%",
+    borderRadius: 20,
+    padding: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
   },
   image: {
-    marginLeft: '8%',
-    marginRight: '8%',
-    width: "60%",
-    height: 200,
+    width: 100,
+    height: 110,
+    borderRadius: 50,
+    marginBottom: 15,
   },
-  carou: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 30
-  },
-  carouselContainer: {
-    marginBottom: 5, // Espacement entre le carousel et les indicateurs
-  },
-  paginationDot: {
-    width: 15,
-    height: 15,
-    borderRadius: 10,
-    marginHorizontal: 8,
-    backgroundColor: '#d95b33',
-  },
-  btn:{
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    marginTop: '15%',
-  }
 });
