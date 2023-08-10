e60f341e11b0b50c29eb6025502ef2d54cb0e101
SamyG <samy.graia@gmail.com>
2023-08-10 16:28:05 +0200

    ok

--- screens/CalibrateScreen.js
+++ screens/CalibrateScreen.js
@@ -119,7 +119,7 @@ const styles = StyleSheet.create({
     marginRight: 10, // Adjusted marginRight
     marginTop: 5,
     marginBottom: 5,
-    borderRadius: 20,
+    borderRadius: 10,
   },
   indicator: {
     backgroundColor: '#d95b33',
