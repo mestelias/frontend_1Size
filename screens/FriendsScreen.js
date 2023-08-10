67f2433aecb3ab7f0bd7655327fa8311fd26df1c
Maxime Huguet~ <huguet.maxime@gmail.com>
2023-08-10 14:23:39 +0200

    friendsscreen work

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -4,7 +4,6 @@ import {
   View,
   Text,
   Image,
-  Button,
   Modal,
   TouchableOpacity,
   TextInput,
@@ -14,6 +13,7 @@ import {
 import { useSelector } from "react-redux";
 
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const url = process.env.EXPO_PUBLIC_IP;
@@ -21,13 +21,12 @@ const url = process.env.EXPO_PUBLIC_IP;
 export default function FriendsScreen({ navigation }) {
   const [friends, setFriends] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [friendProfile, setFriendProfile] = useState(false);
   const [searched, setSearched] = useState(false);
-
 
   const [friendSearch, setFriendSearch] = useState(""); // State pour stocker la valeur entrée dans le champ de recherche
   const [results, setResults] = useState([]); // State pour stocker les résultats de la recherche
-  const [friendAdded, setFriendAdded] = useState(false);
-
+  const [friendSelected, setFriendSelected] = useState({});
 
   const userToken = useSelector((state) => state.user.value.token);
 
@@ -52,29 +51,55 @@ export default function FriendsScreen({ navigation }) {
   };
 
   const renderSearchResults = () => {
-
     {
-      searched && results.length === 0 && <Text style={{ textAlign: 'center', marginTop: 10, fontFamily:"Outfit", fontSize: 16, }}>Aucun utilisateur ne correspond à votre recherche.</Text>
+      searched && results.length === 0 && (
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: 10,
+            fontFamily: "Outfit",
+            fontSize: 16,
+          }}
+        >
+          Aucun utilisateur ne correspond à votre recherche.
+        </Text>
+      );
     }
 
     return results.map((item) => (
       <View
         key={item._id}
-        style={{ flexDirection: "row",width: "100%" ,justifyContent: "space-between" ,alignItems: "center", marginBottom: 10 }}
+        style={{
+          flexDirection: "row",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 10,
+        }}
       >
-        <View style={{ flexDirection: "row", alignItems: "center"}} >
-        {item.image ? (
-          <Image
-            source={{ uri: item.image }}
-            style={{ width: 50, height: 50, borderRadius: 25, marginRight: 20 }}
-          />
-        ) : (
-          <Image
-            source={require("../assets/messi.jpg")}
-            style={{ width: 50, height: 50, borderRadius: 25, marginRight: 20 }}
-          />
-        )}
-        <Text style={styles.text}>{item.username}</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          {item.image ? (
+            <Image
+              source={{ uri: item.image }}
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                marginRight: 20,
+              }}
+            />
+          ) : (
+            <Image
+              source={require("../assets/messi.jpg")}
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                marginRight: 20,
+              }}
+            />
+          )}
+          <Text style={styles.text}>{item.username}</Text>
         </View>
         <TouchableOpacity
           onPress={() => handleAddFriend(item.username)}
@@ -86,42 +111,80 @@ export default function FriendsScreen({ navigation }) {
     ));
   };
 
-  const renderFriendsList = () => {
-    return friends.map((item) => (
+  const mappedFriends = friends.map((item) => {
+    return (
       <View
         key={item.username}
-        style={{ flexDirection: "row",width: "90%" ,justifyContent: "space-between" ,alignItems: "center", marginBottom: 10 }}
+        style={{
+          flexDirection: "row",
+          width: "80%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 10,
+          marginLeft: 30,
+        }}
       >
-        <View style={{ flexDirection: "row", justifyContent: "flex-start", alignItems: "center"}} >
-        {item.image ? (
-          <Image
-            source={{ uri: item.image }}
-            style={{ width: 50, height: 50, borderRadius: 25, marginRight: 15, marginLeft: 15 }}
-          />
-        ) : (
-          <Image
-            source={require("../assets/messi.jpg")}
-            style={{ width: 50, height: 50, borderRadius: 25, marginRight: 15, marginLeft: 15  }}
-          />
-        )}
-        <Text style={styles.text}>{item.username}</Text>
-
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          {item.image ? (
+            <Image
+              source={{ uri: item.image }}
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                marginRight: 15,
+                marginLeft: 15,
+              }}
+            />
+          ) : (
+            <Image
+              source={require("../assets/messi.jpg")}
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                marginRight: 15,
+                marginLeft: 15,
+              }}
+            />
+          )}
+          <TouchableOpacity onPress={() => openFriendProfile(item)}>
+            <Text style={styles.text}>{item.username}</Text>
+          </TouchableOpacity>
         </View>
-
-        <TouchableOpacity style={{ marginRight : 5 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          {/* <TouchableOpacity style={{ marginRight : 5 }}>
           <Text style={styles.text}>Rechercher</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+          <FontAwesome name="shopping-bag" color={"#d95b33"} size={20} />
+
+          {/* <Ionicons size={32} name="close-circle-outline" color="#D95B33" /> */}
+        </View>
       </View>
-    ));
-  };
+    );
+  });
 
   // Cette fonction est appelée lorsque l'utilisateur appuie sur le bouton "Rechercher"
   const handleSearch = async () => {
     // Vérifie que la requête est d'au moins 3 caractères
     if (friendSearch.length < 3) {
-      alert('Veuillez entrer au moins 3 caractères pour effectuer une recherche.');
+      alert(
+        "Veuillez entrer au moins 3 caractères pour effectuer une recherche."
+      );
       return;
-  }
+    }
 
     try {
       const response = await fetch(
@@ -130,7 +193,7 @@ export default function FriendsScreen({ navigation }) {
       const data = await response.json();
       console.log("recherche", data);
       setResults(data);
-      setSearched(true); 
+      setSearched(true);
     } catch (error) {
       console.error("Erreur lors de la recherche d'utilisateurs:", error);
     }
@@ -138,7 +201,7 @@ export default function FriendsScreen({ navigation }) {
 
   // Cette fonction est appelée lorsque l'utilisateur appuie sur le bouton "ajouter"
   const handleAddFriend = async (friendId) => {
-    console.log(friendId)
+    console.log(friendId);
     try {
       const response = await fetch(`${url}/users/addfriend`, {
         method: "POST",
@@ -152,10 +215,12 @@ export default function FriendsScreen({ navigation }) {
       });
       const data = await response.json();
 
-      console.log('data', data)
+      console.log("data", data);
 
       if (data.success) {
         alert(data.message);
+        fetchFriends();
+        closeModal();
       } else {
         alert(data.message);
       }
@@ -165,12 +230,53 @@ export default function FriendsScreen({ navigation }) {
     }
   };
 
+  const handleDeleteFriend = async (friendId) => {
+    console.log(friendId);
+    try {
+      const response = await fetch(`${url}/users/deletefriend`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: userToken,
+          friendId: friendId,
+        }),
+      });
+      const data = await response.json();
+
+      console.log("data", data);
+
+      if (data.success) {
+        alert(data.message);
+        // Rafraîchir la liste des amis après une suppression réussie
+        fetchFriends();
+        setFriendProfile(false);
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("Erreur lors de la suppression d'un ami:", error);
+      alert("Une erreur est survenue.");
+    }
+  };
+
+  const openFriendProfile = (item) => {
+    console.log(item);
+    setFriendProfile(true);
+    setFriendSelected(item);
+  };
+
+  const closeFriendProfile = () => {
+    setFriendProfile(false);
+  };
+
   const closeModal = () => {
     setModalVisible(false);
     setFriendSearch(""); // Réinitialiser le champ de recherche
     setResults([]); // Vider les résultats de la recherche
-    setSearched(false); 
-};
+    setSearched(false);
+  };
 
   return (
     <View style={styles.background}>
@@ -186,14 +292,23 @@ export default function FriendsScreen({ navigation }) {
         <View style={styles.border}></View>
         <View style={styles.container}>
           {/* Bouton pour ouvrir la modal */}
-          <TouchableOpacity 
-          onPress={() => setModalVisible(true)}
-          style={styles.button}
+          <TouchableOpacity
+            onPress={() => setModalVisible(true)}
+            style={styles.button}
           >
             <Text style={styles.textButton}>Ajouter un ami</Text>
           </TouchableOpacity>
-
-          <ScrollView style={{width: "80%"}}showsVerticalScrollIndicator={false}>{renderFriendsList()}</ScrollView>
+          <ScrollView
+            style={{
+              width: "80%",
+              borderTopWidth: 0.5,
+              borderTopColor: "#d95b33",
+              paddingTop: 15,
+            }}
+            showsVerticalScrollIndicator={false}
+          >
+            {mappedFriends}
+          </ScrollView>
         </View>
       </View>
       <Modal
@@ -206,32 +321,94 @@ export default function FriendsScreen({ navigation }) {
       >
         <TouchableWithoutFeedback onPress={closeModal}>
           <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(0,0,0,0.5)', }}
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(0,0,0,0.5)",
+            }}
           >
             <TouchableWithoutFeedback>
               <View style={styles.modalView}>
-              <Text style={styles.h3}>Ajouter un ami</Text>
+                <Text style={styles.h3}>Ajouter un ami</Text>
 
                 <TextInput
                   placeholder="Username"
-                  placeholderTextColor= "#D6D1BD"
+                  placeholderTextColor="#D6D1BD"
                   value={friendSearch}
                   onChangeText={setFriendSearch}
                   style={styles.input}
                 />
-                <TouchableOpacity
-                onPress={handleSearch}
-                style={styles.button}
-                >
+                <TouchableOpacity onPress={handleSearch} style={styles.button}>
                   <Text style={styles.textButton}>Rechercher</Text>
                 </TouchableOpacity>
                 {results.length ? renderSearchResults() : null}
-          
-          {searched && results.length === 0 && (
-            <Text style={{ marginTop: 20, color: "#000000", textAlign: "center" }}>
-              Aucun utilisateur ne correspond à votre recherche.
-            </Text>
-          )}
+
+                {searched && results.length === 0 && (
+                  <Text
+                    style={{
+                      marginTop: 20,
+                      color: "#000000",
+                      textAlign: "center",
+                    }}
+                  >
+                    Aucun utilisateur ne correspond à votre recherche.
+                  </Text>
+                )}
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      <Modal
+        animationType="fade"
+        transparent
+        visible={friendProfile}
+        onRequestClose={() => {
+          setFriendProfile(!friendProfile);
+        }}
+      >
+        <TouchableWithoutFeedback onPress={closeFriendProfile}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(0,0,0,0.5)",
+            }}
+          >
+            <TouchableWithoutFeedback>
+              <View style={styles.modalView}>
+                <View>
+                <Text style={styles.H1}>{friendSelected.username}</Text>
+                <Image
+                  source={require("../assets/messi.jpg")}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    borderRadius: 75,
+                    marginRight: 20,
+                  
+                  }}
+                />
+                </View>
+                <View>
+                <Text style={styles.h3}>Ses vêtements</Text>
+                <View style={styles.orangeLine}></View>
+                </View>
+
+
+                <View>
+
+                </View>
+                <TouchableOpacity
+                  onPress={() => handleDeleteFriend(friendSelected._id)}
+                  style={styles.button2}
+                >
+                  <Text style={styles.textButton2}>
+                    retirer de ma liste d'ami
+                  </Text>
+                </TouchableOpacity>
               </View>
             </TouchableWithoutFeedback>
           </View>
@@ -299,7 +476,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   button: {
-    width: 150,
+    width: 200,
     alignItems: "center",
     marginTop: 30,
     paddingTop: 8,
@@ -322,7 +499,7 @@ const styles = StyleSheet.create({
     width: "90%",
     height: "70%",
     alignItems: "center",
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -335,12 +512,36 @@ const styles = StyleSheet.create({
   h3: {
     color: "#000000",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontFamily: "Outfit",
     marginBottom: 20,
   },
   text: {
     fontFamily: "Outfit",
+    fontSize: 16,
+  },
+  button2: {
+    width: 200,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    marginBottom: 15,
+    backgroundColor: "#d6d1bd",
+    borderRadius: 30,
+    shadowOpacity: 1,
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  textButton2: {
+    color: "#707b81",
+    fontFamily: "Outfit",
+    height: 30,
+    fontWeight: "600",
     fontSize: 16,
   },
 });
